2564d8a625b0905d0cb9ded9f883b68a4880b2d0
VitaliMay <mvt30@mail.ru>
2022-11-20 17:02:54 +0300

    feat: brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length === 0 || bracketsConfig === 0) return false;
+
+  const bracketObj = {}
+  let brasketStr = ''
+  const result = []
+
+  for (let i=0; i<bracketsConfig.length; i++) {
+    bracketObj[bracketsConfig[i][1]] = bracketsConfig[i][0]  // формируем пары зактывающая-открывающая
+    brasketStr = bracketsConfig[i][0] + brasketStr           // формируем строку открывающих
+  }
+
+
+  for (let i=0; i<str.length; i++) {
+    if (result[result.length-1] === bracketObj[str[i]] && result.length > 0)  result.pop(); // если верхняя в стеке и пришедшая составляют пару, то убираем из стека
+      else if (brasketStr.includes(str[i])) result.push(str[i]);                            // если идёт открывающая заносим её в стек
+        else return false;                                                                  // если идёт закрывающая при пустом result или не парная верхней
+  }
+
+  return result.length === 0;                                                               // если остались открывающие
+
 }
